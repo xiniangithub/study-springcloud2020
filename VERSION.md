ed4bd4eeb0db745fd10aedb68e5d2253028a44e5
Admin <2779207210@qq.com>
2021-03-11 22:38:20 +0800

    Eureka集群搭建，provider、consumer注册到Eureka集群

--- VERSION.md
+++ VERSION.md
@@ -6,3 +6,9 @@
   - 消费者订单模块：study-springcloud2020-consumer-order80
   - 公共模块：study-springcloud2020-api-commons
 
+# v1.1
+- Eureka
+  - Eureka Server集群模块1：study-springcloud2020-eureka-server7001
+  - Eureka Server集群模块2：study-springcloud2020-eureka-server7002
+
+- provider consumer 注册到Eureka Server集群中
--- study-springcloud2020-eureka-server7002/src/main/java/com/wez/springcloud/EurekaMain7002.java
+++ study-springcloud2020-eureka-server7002/src/main/java/com/wez/springcloud/EurekaMain7002.java
@@ -2,18 +2,19 @@ package com.wez.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
+ * Eureka Server
  * @Author wez
- * @Date 2021/3/9
+ * @Date 2021/3/11
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7002 {
 
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaMain7002.class, args);
     }
 
 }
