b7cb1fa31e44a90ca39ffcf7914ab113701d61e0
昔小年 <2779207210@qq.com>
2021-03-28 11:23:28 +0800

    消息驱动：Spring Cloud Stream 消费者分组

---
+++ study-springcloud2020-stream-kafka-consumer8803/src/main/java/com/wez/springcloud/KafkaConsumerMain8803.java
@@ -0,0 +1,19 @@
+package com.wez.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * KafkaConsumerMain8803
+ *
+ * @Author wez
+ * @Time 2021/3/28 10:17
+ */
+@SpringBootApplication
+public class KafkaConsumerMain8803 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumerMain8803.class, args);
+    }
+
+}
